e25c240760ef05592d022307d57b24ee8a081734
Tony Kwok <holypaladin07@gmail.com>
2019-07-07 18:35:06 -0700

    retry

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -12,14 +12,11 @@ class SampleTest extends WP_UnitTestCase {
 	}
 
   function testActiveTheme() {
-    var_dump(wp_get_theme());
-    $this->assertTrue( wp_get_theme() == 'basic' );
-		// $this->assertTrue( wp_get_theme()->get_stylesheet() == 'basic' );
+    $this->assertTrue( wp_get_theme() == 'phpunit-demo' );
 	}
 
 	function testInactiveTheme() {
     $this->assertFalse( wp_get_theme() == 'Twenty Twelve' );
-		// $this->assertFalse( wp_get_theme()->get_stylesheet() == 'Twenty Eleven' );
 	}
 
 	function testjQueryIsLoaded() {
@@ -30,6 +27,7 @@ class SampleTest extends WP_UnitTestCase {
 	}
 
 	function testBasicMetaDescription() {
+    var_dump(basic_meta_description());
 		$meta_description = '<meta name="description" content="' . get_bloginfo( 'description' ) . '" />';
 		$this->expectOutputString( $meta_description, basic_meta_description() );
 	}
